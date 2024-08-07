3da7f2faf8bd28bfa606b350510eb83d57ec3bba
MubashirJ <mubashirmjjawed@gmail.com>
2024-08-07 18:31:39 +0500

    theme change

--- lib/ui/theme/color.dart
+++
@@ -1,7 +0,0 @@
- import "package:flutter/material.dart";
- 
- class MainColor{
-  static Color primaryColor = Color(0xff00061a);
-  static Color secondaryColor = Color(0xff001456);
-  static Color accentColor = Color(0xff4169e8);
- }
